33c4087810c45e6d0dc4af78cd5a3b88512b7e56
youssefelhanafi <ysf.elhanafi@gmail.com>
2019-05-03 12:14:42 +0100

    init

---
+++ block_custom_mail.php
@@ -0,0 +1,45 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * filtered_reporting block
+ *
+ * @package    block_filtered_reporting
+ * @author     Youssef Elhanafi <ysf.elhanafi@gmail.com>
+ * @copyright  2019 Youssef Elhanafi 
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+class block_custom_mail extends block_base{
+    
+    public function init(){
+        $this->title = get_string('custom_mail', 'block_custom_mail');
+    }
+    public function get_content() {
+        if ($this->content !== null) {
+          return $this->content;
+        }
+     
+        $this->content         =  new stdClass;
+        $this->content->text   = 'The content of our custom_mail block!';
+        $this->content->footer = 'Footer here...';
+     
+        return $this->content;
+    }
+
+}
---
+++ version.php
@@ -0,0 +1,30 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * filtered_reporting block
+ *
+ * @package    block_filtered_reporting
+ * @author     Youssef Elhanafi <ysf.elhanafi@gmail.com>
+ * @copyright  2019 Youssef Elhanafi 
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'block_custom_mail';  // Recommended since 2.0.2 (MDL-26035). Required since 3.0 (MDL-48494)
+$plugin->version = 2019050301;  // YYYYMMDDHH (year, month, day, 24-hr time)
+$plugin->requires = 2018051700; // YYYYMMDDHH (This is the release version for Moodle 2.0)
